896611a490a005389cb2f5aa56f4d0c1f357772f
david <david@MacBook-Pro-de-david.local>
2021-09-01 12:16:54 +0200

    House DB creado, enlazado e importado

--- src/app/services/casas.service.ts
+++ src/app/services/casas.service.ts
@@ -6,21 +6,11 @@ import { Casa } from '../interfaces/casa.interface';
 })
 export class CasasService {
 
-  arrHouses: Casa[];
+  
 
 
   constructor() { 
-    this.arrHouses = [{
-      id: 1,
-      titulo: "Bonita casa adosada",
-      propietario: "Don Ramiro",
-      direccion: "Calle del Almendro 18, 18291",
-      ciudad: 'Madrid',
-      numerohabitaciones: 3,
-      lat: 182912,
-      long: 1289712,
-      disponibilidad: true,
-      foto: ""
-    }]
+
+
   }
 }
