d8e9f34f065f253890cb9844d507b3a274c5dd0d
david <david@MacBook-Pro-de-david.local>
2021-09-01 13:41:49 +0200

    servicio devolviendo getAll

--- src/app/services/casas.service.ts
+++ src/app/services/casas.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Casa } from '../interfaces/casa.interface';
+import { HOUSES } from './house.db';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CasasService {
 
-  
+  arrCasas: Casa[] = [];
 
 
   constructor() { 
+    this.arrCasas = HOUSES;
+  }
 
-
+  getAll(){
+    return HOUSES;
   }
 }
