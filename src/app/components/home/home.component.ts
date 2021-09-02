78f4bdd253467ec4a9fa5da7f24db08d1cea8321
david <david@MacBook-Pro-de-david.local>
2021-09-02 13:59:06 +0200

    Componente home actualizado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Casa } from 'src/app/interfaces/casa.interface';
+import { Observable } from 'rxjs';
+// import { Casa } from 'src/app/interfaces/casa.interface';
 import { CasasService } from 'src/app/services/casas.service';
 
 @Component({
@@ -9,10 +10,9 @@ import { CasasService } from 'src/app/services/casas.service';
 })
 export class HomeComponent implements OnInit {
 
-  arrCasas: Casa[];
+  arrCasas: Observable<any[]> = new Observable;
 
   constructor(private casasService: CasasService) { 
-    this.arrCasas = [];
   }
 
   ngOnInit() {
